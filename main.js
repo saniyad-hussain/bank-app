c59b510d1b3f92167989f12114677d7201604989
saniyad-hussain <chowdhurysaniayd@gmail.com>
2020-07-22 20:04:50 -0400

    deposit and balance update

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ loginButton.addEventListener("click", function(){
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 });
+
+const depositButton = document.getElementById("deposit-btn");
+depositButton.addEventListener("click", function(){
+    const depositAmount = document.getElementById("dep-amount").value;
+    const depositAmountNumber = parseFloat(depositAmount);
+
+    const newDeposit = document.getElementById("new-deposit").innerText;
+    const newDepositNumber = parseFloat(newDeposit);
+    const totalDeposit = depositAmountNumber + newDepositNumber;
+    document.getElementById("new-deposit").innerText = totalDeposit;
+
+    const newBalance = document.getElementById("new-balance").innerText;
+    const newBalanceNumber = parseFloat(newBalance);
+    const updatedBalance = depositAmountNumber + newBalanceNumber;
+    document.getElementById("new-balance").innerText = updatedBalance;
+
+});
+
